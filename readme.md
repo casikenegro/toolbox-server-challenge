63b82d147aeb574b5ffc1735ab16bf76f2c269a6
casikenegro <ortizjesusrivas@gmail.com>
2023-04-10 06:51:51 -0400

    feat: add readme.md

---
+++ readme.md
@@ -0,0 +1,30 @@
+# ToolBox server  Challenge
+
+## Installation
+
+### local dev
+
+```bash
+npm install
+```
+### run with nodemon
+```bash
+npm run start-dev
+```
+### run with node 
+```bash
+npm run start
+```
+### with Docker 
+
+```bash
+docker build . -p 3000:3000 -t toolbox-server:lastet
+```
+
+## link
+
+https://toolbox-server-test.onrender.com
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/controllers/files.js
+++ src/controllers/files.js
@@ -3,12 +3,11 @@ const { csvJSON } = require("../utils");
 
 const getFileByName = async (req, res) => {
   try {
-    console.log(req.query.name);
-    const response = await axios.get(
-      `https://echo-serv.tbxnet.com/v1/secret/file/${req.query.name}`,
-      { responseType: "blob" },
-      { headers: { Authorization: `Bearer aSuperSecretKey` } }
-    );
+    const response = await axios({
+      url:`https://echo-serv.tbxnet.com/v1/secret/file/${req.query.name}`,
+      responseType: "blob" ,
+      headers: { Authorization: `Bearer aSuperSecretKey` }
+    })
     return res.send({ file: req.query.name, lines: csvJSON(response.data) });
   } catch (error) {
     return res.status(500).send({ message: "server error" });
